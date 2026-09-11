415bad2e5e461c7859e5d2958c3b841f53472409
rsmrtk <martun5@icloud.com>
2026-09-11 11:08:04 +0300

    Initial Commit

---
+++ finance-rsmrtk/finance_rsmrtkApp.swift
@@ -0,0 +1,17 @@
+//
+//  finance_rsmrtkApp.swift
+//  finance-rsmrtk
+//
+//  Created by rsmrtk on 11.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct finance_rsmrtkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
